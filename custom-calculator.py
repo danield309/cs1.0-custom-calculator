d1fa3899d18dc32f4ad717d11ede5a49ef3655af
daniel <duque1daniel@gmail.com>
2020-08-27 23:02:45 -0700

    added calculator program & commented code

---
+++ custom-calculator.py
@@ -0,0 +1,7 @@
+print("This program will help you calculate your car's mpg.")
+miles_driven = input("Enter miles driven: ") #prompts user to input miles driven
+miles_driven = float(miles_driven)  #converts input into a float
+gallons_used = input("Enter gallons used: ") #prompts user to input gallons used
+gallons_used = float(gallons_used) #converts input into a float
+mpg = miles_driven / gallons_usedn #the variable mpg is assigned the qoutient of miles & gallons used
+print("Estimated miles per galon:" , mpg) #returns your mpg
