eacb058832c7c9777e6b551275c006af918df47e
daniel <duque1daniel@gmail.com>
2020-08-31 15:07:31 -0700

    fixed a bug on line 7

--- custom-calculator.py
+++ custom-calculator.py
@@ -3,5 +3,5 @@ miles_driven = input("Enter miles driven: ") #prompts user to input miles driven
 miles_driven = float(miles_driven)  #converts input into a float
 gallons_used = input("Enter gallons used: ") #prompts user to input gallons used
 gallons_used = float(gallons_used) #converts input into a float
-mpg = miles_driven / gallons_usedn #the variable mpg is assigned the qoutient of miles & gallons used
-print("Estimated miles per galon:" , mpg) #returns your mpg
+mpg = miles_driven / gallons_used #the variable mpg is assigned the qoutient of miles & gallons entered
+print("Estimated miles per galon: " + str(mpg) + "mpg") #returns your mpg
